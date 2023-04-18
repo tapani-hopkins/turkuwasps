8c08168de3ee790621b66faf656b522317676ceb
tapani-hopkins <tapani-hopkins@users.noreply.github.com>
2023-04-18 10:07:08 +0300

    update documentation

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -2,7 +2,7 @@
 #'
 #' This is a helper function used by [plot_place()]. Gives defaults for what bars to include in the barplot, what order to place them in, and what colour to use.
 #'
-#' @param x Vector of which trap, site or other location each wasp came from. Either as strings or factor.
+#' @param x Vector of which trap, forest type or other location each wasp came from. Either as strings or factor.
 #'
 #' @return List with items `x` (locations converted to factor, with the factor levels telling what bars to plot) and `colour` (the colours to give each bar). 
 #'
@@ -18,7 +18,7 @@ get_defaults = function(x){
 		
 	# ..stop if it is unclear what the locations are
 	} else {
-		stop("Could not figure out if the locations were traps or sites. Check that thye are either all traps or all sites, and that every one is valid.")
+		stop("Could not figure out if the locations were traps or forest types. Check that they are either all traps or all forest types, and that every one is valid.")
 	}	
 		
 	# only include collecting events that are in `x`
--- R/plot_place.R
+++ R/plot_place.R
@@ -1,8 +1,8 @@
 #' Plot wasps per place
 #'
-#' Barplot the number of wasps caught in each trap, site, or other location. This is basically a wrapper for two base R functions: [table()] for counting how many wasps belong to each bar, and [barplot()] to draw the plot.
+#' Barplot the number of wasps caught in each trap, forest type, or other location. This is basically a wrapper for two base R functions: [table()] for counting how many wasps belong to each bar, and [barplot()] to draw the plot.
 #'
-#' @param x Vector of which trap, site or other location each wasp came from. Either as strings or factor. (If factor, and `defaults`=FALSE, one bar is plotted for each factor level, in the same order as the levels)
+#' @param x Vector of which trap, forest type or other location each wasp came from. Either as strings or factor. (If factor, and `defaults`=FALSE, one bar is plotted for each factor level, in the same order as the levels)
 #' @param taxon Vector giving the taxon of each wasp. If given, splits the bars to show how many wasps of each taxon were caught.
 #' @param weights How much to scale each bar by. Vector of same length as the number of bars, or can also be a single number. Typically used to scale down bars by sampling effort.
 #' @param defaults If TRUE, uses default settings for what bars to show, and for the order and colour of the bars. For example, if `x` contains Ugandan traps, draws one bar for each Ugandan trap, in successional order (primary forest to farm), with primary forest in dark green, swamp in blue etc.

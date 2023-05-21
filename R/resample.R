4ce3f315699a7075852824b089e8c909062840e3
tapani-hopkins <tapani-hopkins@users.noreply.github.com>
2023-05-21 13:35:33 +0300

    update draft resample()

--- R/resample.R
+++ R/resample.R
@@ -5,6 +5,8 @@
 #' @param model Character string giving the model to fit. Generally something like "offset(tdiff_log) + days + rain + forest_type + deadwood". The variables should be found as column names in `m`. Converted to [formula], and will accept e.g. interaction terms in the same format as for formulae.
 #' @param x Data frame with the wasp data. Must contain columns "sample" and "taxon". 
 #' @param m Data frame with the Malaise sample data. Must contain columns "name" and "event".
+#' @param pairwise Character vector of the columns in `m` for which pairwise p values should be calculated. E.g. `pairwise = "forest_type"` will check what forest types differ significantly from each other, not just if significant differences exist between forest types. Should only be used for categorical variables such as forest type, trap etc; not numeric such as rainfall. If NULL, only checks if significant differences exist. 
+#' @param family Probability distribution used to fit the model. Passed to [manyglm()]. In general, this will be "negative.binomial" or "poisson".
 #' @param ...  Other parameters passed to [anova.manyglm()], and [summary.manyglm()]. These will override any default parameters. In general, there will be little need to adjust anything except `nBoot` (number of resamples). It is nine by default, to give a quick but approximate result. More accurate results will need e.g. `nBoot=499`, which can take several hours to finish.  
 #' 
 #' @return List with items:
@@ -12,7 +14,7 @@
 #' * fit Fitted values for each sample and taxon.
 #' 
 #' @export
-resample = function(model, x, m, ...){
+resample = function(model, x, m, pairwise=NULL, family="negative.binomial", ...){
 	
 	# store various default arguments for the analysis
 	analysis_args = list(
@@ -45,12 +47,20 @@ resample = function(model, x, m, ...){
 	mv = table(x$sample, x$taxon)
 
 	# fit the model to the data
-	fit = mvabund::manyglm(model, data=m, family="negative.binomial")
+	fit = mvabund::manyglm(model, data=m, family=family)
 	
 	# add the fitted model to the analysis arguments
 	analysis_args$object = fit
 	
-	a = do.call(mvabund::anova.manyglm, args=analysis_args)
+	# 
+	if (is.null(pairwise)){
+		a = do.call(mvabund::anova.manyglm, args=analysis_args)
+	
+	#
+	} else {
+		
+	}
+	
 	#a = anova(fit, test = "LR", resamp="pit.trap", p.uni="unadjusted", nBoot=9)
 	#s = summary(mg1, test = "LR", resamp="pit.trap", p.uni="unadjusted", nBoot=9)
 	

a9215adf79c156bae8d69eff95f6ff52f61bb984
tapani-hopkins <tapani-hopkins@users.noreply.github.com>
2025-08-09 20:57:20 +0300

    add smooth_data()

--- R/misc_functions.R
+++ R/misc_functions.R
@@ -167,3 +167,80 @@ relevel0 = function(x, ref){
 	
 }
 
+
+#' Smooth data that has been collected during datetime intervals
+#'
+#' Smooth e.g. weather data that has been collected at specific time intervals. Useful to e.g. show average rainfall.
+#'
+#' @param x Numeric vector of data values, such as rainfall per day, or temperature.
+#' @param xdate Vector of intervals (see [as.interval()]) of same length as `x`, giving the times when the data was collected.
+#' @param k Number of days over which to smooth the data. Default (28) is to take the average of the past 28 days. Larger numbers give smoother results.
+#' @param p Minimum portion of the data that is required. Filters out random spikes at the start and end of when there is data. Default is to return NA if less than 25% of the period over which the data is averaged has data. 
+#' @param xlim Interval object giving the start and end of the time period for which smoothed data is wanted. If not given, the smoothed data will cover the entire period from which there is data.
+#' @param step Desired resolution (in seconds) of the smoothed data. Default is to return a smoothed data point every 24 hours.
+#'
+#' @return List giving the time intervals and values of the smoothed data. The list has two items: 
+#' * `x` Vector of intervals.
+#' * `y` Vector of smoothed data.
+#'
+#' @details
+#' The default smoothing takes the average of the past 28 days' data, at one-day intervals. Data that was collected at the start or end of the 28 day period is weighted down: if e.g. only half of the time period during which the data was collected is in the 28-day period, it'll have a 50% weight in the weighted average.
+#' 
+#' @examples
+#' # get the start and end times of weather data
+#' wdate = as.interval(weather$start, weather$end)
+#' 
+#' # get the amount of rain per day
+#' rain = weather$rain / tdiff(wdate) * 3600 * 24
+#'
+#' # smooth the rain data
+#' r = smooth_data(rain, wdate)
+#' 
+#' # plot the raw data as points, and the smoothed data as a line
+#' xlim = as.interval(min(wdate), max(wdate))
+#' plot(wdate$e, rain, xlim=xlim, pch=20, cex=0.1, col="blue")
+#' lines(r$x, r$y, xlim=xlim, col="blue")
+#'
+#' @export
+#'
+smooth_data = function(x, xdate, k=28, p=0.25, xlim=NULL, step=3600*24){
+	
+	# if xlim wasn't given, span the whole time period of the data
+	if (is.null(xlim)){
+		xlim = as.interval(min(xdate), max(xdate))
+	}
+	
+	# create data points for the smoothed data at `step` second intervals
+	px = seq(xlim$s, xlim$e, step=step)
+	px_k = as.interval(px - 3600*24*k, px)
+	
+	# create variable for the smoothed data
+	y = rep(NA, length(px))
+	
+	# Smooth the data and add to each smoothed data point
+	for (i in 1:length(px_k)){
+		
+		# count how much each data point overlaps with this time period
+		over = overlap(xdate, px_k[i])
+		
+		# get the weights of each data point (0: not in time period, 1: entirely in time period)
+		weight = over / tdiff(xdate)
+		
+		# check how large a part of the time period has data
+		coverage = sum(over) / tdiff(px_k[i])
+		
+		# take the weighted average of the time period's data if there's enough data..
+		if (coverage >= p){
+			y[i] = stats::weighted.mean(x, w=weight, na.rm=TRUE)
+		
+		# .. if there's not enough data, set to NA
+		} else {
+			y[i] = NA
+		}
+		
+	}
+	
+	# return the smoothed data and their time interval
+	return(list(x=px, y=y))
+	
+}
